dd7d5cf3df2f4a9ff34a639b499fe64eca039cac
aja512 <42480995+aja512@users.noreply.github.com>
2022-07-02 23:35:37 +0530

    Commit

---
+++ Scrumdinger/Scrumdinger/TrailingLabel.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingLabel.swift
+//  Scrumdinger
+//
+//  Created by Amanda Judy Andrade on 01/07/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
