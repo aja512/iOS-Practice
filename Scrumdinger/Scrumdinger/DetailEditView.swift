e4e773a7da44954302da86f1a1ede492ba3c6571
aja512 <ajandrade25@gmail.com>
2022-07-06 23:21:43 +0530

    initial commit

---
+++ Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Amanda Judy Andrade on 06/07/22.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
